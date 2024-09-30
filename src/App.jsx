705001526463521c1ba68e4a06fe34fceec49e39
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2024-09-30 12:05:56 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes,Route } from 'react-router-dom'
 import './App.css'
-import { Suspense, useEffect } from 'react'
+import { Suspense, useEffect,lazy } from 'react'
 import Layout from './components/Layout/Layout'
-import HomePage from './pages/HomePage/HomePage'
-import ContactsPage from './pages/ContactsPage/ContactsPage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
-import LogInPage from './pages/LogInPage/LogInPage'
 import { useDispatch, useSelector } from 'react-redux'
 import { setIsRefreshing } from './redux/auth/selectors'
 import { refreshUser } from './redux/auth/operations'
 import RestrictedRoute from './components/RestrictedRoute'
 import PrivateRoute from './components/PrivateRoute'
-// import PrivateRoute from "./PrivateRoute";
+const HomePage=lazy(()=>import('../src/pages/HomePage/HomePage'))
+const ContactsPage =lazy(()=>import('../src/pages/ContactsPage/ContactsPage'))
+const RegisterPage=lazy(()=>import('../src/pages/RegisterPage/RegisterPage'))
+const LogInPage =lazy(()=>import('../src/pages/LogInPage/LogInPage'))
+
 function App() {
 const dispatch=useDispatch();
 const isUserRefreshing=useSelector(setIsRefreshing)
